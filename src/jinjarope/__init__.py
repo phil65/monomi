33584a2e548b38c92c57ef3a3d3e0407a7dc9320
phil65 <philipptemminghoff@googlemail.com>
2023-11-03 22:05:46 +0100

    chore: add stuff to __init__

--- src/jinjarope/__init__.py
+++ src/jinjarope/__init__.py
@@ -1,1 +1,22 @@
 __version__ = "0.0.3"
+
+
+from environment import Environment
+from loaders import (
+    FileSystemLoader,
+    FsSpecFileSystemLoader,
+    FsSpecProtocolPathLoader,
+    ChoiceLoader,
+    PackageLoader,
+    DictLoader,
+)
+
+__all__ = [
+    "Environment",
+    "FsSpecFileSystemLoader",
+    "FsSpecProtocolPathLoader",
+    "FileSystemLoader",
+    "ChoiceLoader",
+    "PackageLoader",
+    "DictLoader",
+]
