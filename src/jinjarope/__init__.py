e2ca7958e5595f72a76bb60b24f312e47e064fd7
phil65 <philipptemminghoff@googlemail.com>
2023-11-04 03:29:28 +0100

    chore: add some stuff to root __init__

--- src/jinjarope/__init__.py
+++ src/jinjarope/__init__.py
@@ -1,13 +1,14 @@
 __version__ = "0.0.4"
 
 
-from .environment import Environment
+from .environment import BlockNotFoundError, Environment
 from .loaders import (
     FileSystemLoader,
     FsSpecFileSystemLoader,
     FsSpecProtocolPathLoader,
     ChoiceLoader,
     PackageLoader,
+    PrefixLoader,
     DictLoader,
 )
 from .loaderregistry import LoaderRegistry
@@ -18,12 +19,14 @@ registry = LoaderRegistry()
 get_loader = registry.get_loader
 
 __all__ = [
+    "BlockNotFoundError",
     "Environment",
     "FsSpecFileSystemLoader",
     "FsSpecProtocolPathLoader",
     "FileSystemLoader",
     "ChoiceLoader",
     "PackageLoader",
+    "PrefixLoader",
     "DictLoader",
     "get_loader",
 ]
