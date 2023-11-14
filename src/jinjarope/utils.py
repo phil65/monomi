929ed7f9b2105acb2787bb41b0cfe1d389155388
phil65 <philipptemminghoff@googlemail.com>
2023-11-14 03:06:11 +0100

    chore: docstrings

--- src/jinjarope/utils.py
+++ src/jinjarope/utils.py
@@ -42,6 +42,11 @@ def get_repr(_obj: Any, *args: Any, **kwargs: Any) -> str:
 
 @functools.cache
 def fsspec_get(path: str) -> str:
+    """Fetch a file via fsspec and return file content as a string.
+
+    Arguments:
+        path: The path to fetch the file from
+    """
     import fsspec
 
     with fsspec.open(path) as file:
@@ -51,17 +56,29 @@ def fsspec_get(path: str) -> str:
 T = TypeVar("T")
 
 
-def reduce_list(data_set: Iterable[T]) -> list[T]:
-    """Reduce duplicate items in a list and preserve order."""
-    return list(dict.fromkeys(data_set))
+def reduce_list(items: Iterable[T]) -> list[T]:
+    """Reduce duplicate items in a list and preserve order.
+
+    Arguments:
+        items: The iterable to recude to a unique-item list
+    """
+    return list(dict.fromkeys(items))
 
 
-def flatten_dict(dct: Mapping, sep: str = "/", parent_key: str = "") -> Mapping:
+def flatten_dict(dct: Mapping, sep: str = "/", _parent_key: str = "") -> Mapping:
+    """Flatten a nested dictionary to a flat one.
+
+    The individual parts of the "key path" are joined with given separator.
+
+    Arguments:
+        dct: The dictionary to flatten
+        sep: The separator to use for joining
+    """
     items: list[tuple[str, str]] = []
     for k, v in dct.items():
-        new_key = parent_key + sep + k if parent_key else k
+        new_key = _parent_key + sep + k if _parent_key else k
         if isinstance(v, Mapping):
-            items.extend(flatten_dict(v, parent_key=new_key, sep=sep).items())
+            items.extend(flatten_dict(v, _parent_key=new_key, sep=sep).items())
         else:
             items.append((new_key, v))
     return dict(items)
@@ -69,6 +86,11 @@ def flatten_dict(dct: Mapping, sep: str = "/", parent_key: str = "") -> Mapping:
 
 @functools.lru_cache(maxsize=1)
 def _get_black_formatter() -> Callable[[str, int], str]:
+    """Return a formatter.
+
+    If black is available, a callable to format code using black is returned,
+    otherwise a noop callable is returned.
+    """
     try:
         from black import InvalidInput, Mode, format_str
     except ModuleNotFoundError:
@@ -86,6 +108,12 @@ def _get_black_formatter() -> Callable[[str, int], str]:
 
 
 def format_code(code: str, line_length: int = 100):
+    """Format code to given line length using `black`.
+
+    Arguments:
+        code: The code to format
+        line_length: Line length limit for formatted code
+    """
     code = code.strip()
     if len(code) < line_length:
         return code
