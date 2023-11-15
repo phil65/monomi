8d168e3b4b4b5139d333899b41a45f04bd48d848
phil65 <philipptemminghoff@googlemail.com>
2023-11-16 00:43:39 +0100

    refactor: move filters to separate modules

--- src/jinjarope/envglobals.py
+++ src/jinjarope/envglobals.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Sequence
 import configparser
 import datetime
 import functools
@@ -20,7 +20,7 @@ import sys
 import tomllib
 from typing import Any, Literal
 
-from jinjarope import envtests, utils
+from jinjarope import envtests, htmlfilters, iterfilters, utils
 
 
 logger = logging.getLogger(__name__)
@@ -34,69 +34,6 @@ version_info = dict(
     architecture=platform.architecture(),
     python_implementation=platform.python_implementation(),
 )
-
-
-def wrap_in_elem(
-    text: str | None,
-    tag: str,
-    add_linebreaks: bool = False,
-    **kwargs,
-) -> str:
-    """Wrap given text in an HTML/XML tag (with attributes).
-
-    If text is empty, just return an empty string.
-
-    Arguments:
-        text: Text to wrap
-        tag: Tag to wrap text in
-        add_linebreaks: Adds a linebreak before and after the text
-        kwargs: additional key-value pairs to be inserted as attributes for tag.
-                Key strings will have "_" stripped from the end to allow using keywords.
-    """
-    if not text:
-        return ""
-    attrs = [f'{k.rstrip("_")}="{v}"' for k, v in kwargs.items()]
-    attr_str = (" " + " ".join(attrs)) if attrs else ""
-    nl = "\n" if add_linebreaks else ""
-    return f"<{tag}{attr_str}>{nl}{text}{nl}</{tag}>"
-
-
-def html_link(text: str | None = None, link: str | None = None, **kwargs) -> str:
-    """Create a html link.
-
-    If link is empty string or None, just the text will get returned.
-
-    Arguments:
-        text: Text to show for the link
-        link: Target url
-        kwargs: additional key-value pairs to be inserted as attributes.
-                Key strings will have "_" stripped from the end to allow using keywords.
-    """
-    if not link:
-        return text or ""
-    attrs = [f'{k.rstrip("_")}="{v}"' for k, v in kwargs.items()]
-    attr_str = (" " + " ".join(attrs)) if attrs else ""
-    return f"<a href={link!r}{attr_str}>{text or link}</a>"
-
-
-def md_link(
-    text: str | None = None,
-    link: str | None = None,
-    tooltip: str | None = None,
-) -> str:
-    """Create a markdown link.
-
-    If link is empty string or None, just the text will get returned.
-
-    Arguments:
-        text: Text to show for the link
-        link: Target url
-        tooltip: Optional tooltip
-    """
-    if not link:
-        return text or ""
-    tt = f" '{tooltip}'" if tooltip else ""
-    return f"[{text or link}]({link}{tt})"
 
 
 def serialize(data: Any, mode: Literal["yaml", "json", "ini", "toml"] | None) -> str:  # type: ignore[return]
@@ -155,78 +92,11 @@ def get_output_from_call(
     if not isinstance(call, str):
         call = " ".join(call)
     try:
-        return subprocess.run(
-            call,
-            stdout=subprocess.PIPE,
-            text=True,
-            shell=True,
-            cwd=cwd,
-        ).stdout
+        pipe = subprocess.PIPE
+        return subprocess.run(call, stdout=pipe, text=True, shell=True, cwd=cwd).stdout
     except subprocess.CalledProcessError:
         logger.warning("Executing %s failed", call)
         return None
-
-
-def format_js_map(mapping: dict | str, indent: int = 4) -> str:
-    """Return JS map str for given dictionary.
-
-    Arguments:
-        mapping: Dictionary to dump
-        indent: The amount of indentation for the key-value pairs
-    """
-    dct = json.loads(mapping) if isinstance(mapping, str) else mapping
-    rows = []
-    indent_str = " " * indent
-    for k, v in dct.items():
-        match v:
-            case bool():
-                rows.append(f"{indent_str}{k}: {str(v).lower()},")
-            case dict():
-                rows.append(f"{indent_str}{k}: {format_js_map(v)},")
-            case None:
-                rows.append(f"{indent_str}{k}: null,")
-            case _:
-                rows.append(f"{indent_str}{k}: {v!r},")
-    row_str = "\n" + "\n".join(rows) + "\n"
-    return f"{{{row_str}}}"
-
-
-def svg_to_data_uri(svg: str) -> str:
-    """Wrap svg as data URL.
-
-    Arguments:
-        svg: The svg to wrap into a data URL
-    """
-    return f"url('data:image/svg+xml;charset=utf-8,{ svg }')"
-
-
-def format_css_rule(dct: Mapping) -> str:
-    """Format a nested dictionary as CSS rule.
-
-    Mapping must be of shape {".a": {"b": "c"}}
-
-    Arguments:
-        dct: The mapping to convert to CSS text
-    """
-    data: dict[str, list] = {}
-
-    def _parse(obj, selector: str = ""):
-        for key, value in obj.items():
-            if hasattr(value, "items"):
-                rule = selector + " " + key
-                data[rule] = []
-                _parse(value, rule)
-
-            else:
-                prop = data[selector]
-                prop.append(f"\t{key}: {value};\n")
-
-    _parse(dct)
-    string = ""
-    for key, value in sorted(data.items()):
-        if data[key]:
-            string += key[1:] + " {\n" + "".join(value) + "}\n\n"
-    return string
 
 
 def add(text, prefix: str = "", suffix: str = ""):
@@ -279,23 +149,6 @@ def ternary(value: Any, true_val: Any, false_val: Any, none_val: Any = None):
     if bool(value):
         return true_val
     return false_val
-
-
-def do_any(seq: Sequence, attribute: str | None = None) -> bool:
-    """Check if at least one of the item in the sequence evaluates to true.
-
-    The `any` builtin as a filter for Jinja templates.
-
-    Arguments:
-        seq: An iterable object.
-        attribute: The attribute name to use on each object of the iterable.
-
-    Returns:
-        A boolean telling if any object of the iterable evaluated to True.
-    """
-    if attribute is None:
-        return any(seq)
-    return any(_[attribute] for _ in seq)
 
 
 def resolve(name: str, module: str | None = None):
@@ -355,37 +208,46 @@ ENV_GLOBALS = {
     "environment": version_info,
 }
 ENV_FILTERS = {
+    # Format filters
+    "repr": repr,
     "pformat": pprint.pformat,
     "format_code": utils.format_code,
-    "html_link": html_link,
-    "md_link": md_link,
-    "wrap_in_elem": wrap_in_elem,
-    "repr": repr,
+    # HTML filters
+    "html_link": htmlfilters.html_link,
+    "md_link": htmlfilters.md_link,
+    "wrap_in_elem": htmlfilters.wrap_in_elem,
+    "format_js_map": htmlfilters.format_js_map,
+    "format_css_rule": htmlfilters.format_css_rule,
+    "svg_to_data_uri": htmlfilters.svg_to_data_uri,
+    "clean_svg": htmlfilters.clean_svg,
+    # Iter filters
+    "batched": iterfilters.batched,
+    "reduce_list": iterfilters.reduce_list,
+    "flatten_dict": iterfilters.flatten_dict,
+    "pairwise": itertools.pairwise,
     "zip": zip,
-    "any": do_any,
+    "any": iterfilters.do_any,
+    # Text modification filters
     "rstrip": str.rstrip,
     "lstrip": str.lstrip,
     "removesuffix": str.removesuffix,
     "removeprefix": str.removeprefix,
-    "contains": operator.contains,
     "regex_replace": regex_replace,
+    # serialization filters
+    "dump_json": json.dumps,
+    "load_json": json.loads,
+    "load_toml": tomllib.loads,
+    "serialize": serialize,
+    # misc
     "add": add,
-    "pairwise": itertools.pairwise,
     "ternary": ternary,
     "issubclass": is_subclass,
     "isinstance": is_instance,
     "import_module": importlib.import_module,
     "hasattr": hasattr,
     "partial": functools.partial,
-    "dump_json": json.dumps,
-    "load_json": json.loads,
-    "load_toml": tomllib.loads,
-    "serialize": serialize,
     "load_file": load_file_cached,
     "path_join": os.path.join,
-    "format_js_map": format_js_map,
-    "format_css_rule": format_css_rule,
-    "svg_to_data_uri": svg_to_data_uri,
     "check_output": get_output_from_call,
     "getenv": os.getenv,
 }
