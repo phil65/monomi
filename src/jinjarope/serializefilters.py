fc7b0d958c3b89275ac37607b7040a593202abf0
phil65 <philipptemminghoff@googlemail.com>
2023-11-18 05:15:54 +0100

    feat: add dig filter

--- src/jinjarope/serializefilters.py
+++ src/jinjarope/serializefilters.py
@@ -60,3 +60,49 @@ def deserialize(data: str, fmt: Literal["yaml", "json", "ini", "toml"] | None) -
             return tomllib.loads(data)
         case _:
             raise TypeError(fmt)
+
+
+def dig(
+    data: dict,
+    *sections: str,
+    keep_path: bool = False,
+    dig_yaml_lists: bool = True,
+):
+    """Try to get data with given section path from a dict-list structure.
+
+    If a list is encountered and dig_yaml_lists is true, treat it like a list of
+    {"identifier", {subdict}} items, as used in MkDocs config for
+    plugins & extensions.
+    If Key path does not exist, return None.
+
+    Arguments:
+        data: The data to dig into
+        sections: Sections to dig into
+        keep_path: Return result with original nesting
+        dig_yaml_lists: Also dig into single-key->value pairs, as often found in yaml.
+    """
+    for i in sections:
+        if isinstance(data, dict):
+            if child := data.get(i):
+                data = child
+            else:
+                return None
+        elif dig_yaml_lists and isinstance(data, list):
+            # this part is for yaml-style listitems
+            for idx in data:
+                if i in idx and isinstance(idx, dict):
+                    data = idx[i]
+                    break
+                if isinstance(idx, str) and idx == i:
+                    data = idx
+                    break
+            else:
+                return None
+    if not keep_path:
+        return data
+    result: dict[str, dict] = {}
+    new = result
+    for sect in sections:
+        result[sect] = data if sect == sections[-1] else {}
+        result = result[sect]
+    return new
