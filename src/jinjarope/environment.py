fbc6b1041fd5fae1aa9e48c58745b51bf6659495
phil65 <philipptemminghoff@googlemail.com>
2023-11-23 02:31:41 +0100

    feat: add humanize filters

--- src/jinjarope/environment.py
+++ src/jinjarope/environment.py
@@ -78,6 +78,7 @@ class Environment(jinja2.Environment):
         self.load_jinja_file(folder / "filters.toml")
         self.load_jinja_file(folder / "tests.toml")
         self.load_jinja_file(folder / "functions.toml")
+        self.load_jinja_file(folder / "humanize_filters.toml")
         self.globals.update(envglobals.ENV_GLOBALS)
         for fn in utils._entry_points("jinjarope.environment").values():
             fn(self)
