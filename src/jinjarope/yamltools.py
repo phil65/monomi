ff3acb826e5a972a1006218094a7b86eb7082634
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-11-01 13:45:29 +0100

    chore: cleanup

--- src/jinjarope/yamltools.py
+++ src/jinjarope/yamltools.py
@@ -1,7 +1,10 @@
+"""YAML handling utilities with enhanced loading and dumping capabilities."""
+
 from __future__ import annotations
 
 import collections
-from typing import TYPE_CHECKING, Any, Literal, TypeVar
+import os
+from typing import Any, Literal, TypeVar
 
 import fsspec
 from mknodes.utils import log
@@ -10,77 +13,94 @@ import yaml_env_tag
 import yaml_include
 
 
-if TYPE_CHECKING:
-    import os
-
-
 logger = log.get_logger(__name__)
 
 LoaderStr = Literal["unsafe", "full", "safe"]
 LoaderType = type[yaml.Loader | yaml.CLoader]
 DumperType = type[yaml.Dumper | yaml.CDumper]
-
-# this reference allows other libs to only import jinjarope directly.
-YamlError = yaml.YAMLError
-
+YamlError = yaml.YAMLError  # Reference for external libs
+LOADERS: dict = {
+    "unsafe": yaml.CUnsafeLoader,
+    "full": yaml.CFullLoader,
+    "safe": yaml.CSafeLoader,
+}
 T = TypeVar("T", bound=type)
 
 
 def create_subclass(base_cls: T) -> T:
-    """Create a subclass of the given base class.
+    """Create a subclass of the given base class to avoid modifying original classes.
 
     Args:
-        base_cls: The base class to inherit from
+        base_cls: Base class to inherit from
 
     Returns:
-        A new subclass of the base class
+        New subclass of the base class
     """
-
-    class SubClass(base_cls):  # type: ignore[valid-type]
-        """Subclass, used to not modify the pyyaml classes themselves."""
-
-    return SubClass  # type: ignore[return-value]
+    return type("SubClass", (base_cls,), {})  # type: ignore[return-value]
 
 
 def get_include_constructor(
     fs: str | os.PathLike[str] | fsspec.AbstractFileSystem | None = None,
     **kwargs: Any,
-):
-    """Wraps yaml_include.Constructor for allowing a string for filesystem."""
+) -> yaml_include.Constructor:
+    """Create a YAML include constructor with filesystem support.
+
+    Args:
+        fs: Filesystem specification (path or filesystem object)
+        kwargs: Additional arguments for the Constructor
+
+    Returns:
+        Configured YAML include constructor
+    """
     match fs:
-        case str():
-            filesystem, _ = fsspec.url_to_fs(fs)
+        case str() | os.PathLike():
+            filesystem, _ = fsspec.url_to_fs(str(fs))
         case None:
             filesystem = fsspec.filesystem("file")
         case fsspec.AbstractFileSystem():
             filesystem = fs
+        case _:
+            msg = f"Unsupported filesystem type: {type(fs)}"
+            raise TypeError(msg)
+
     return yaml_include.Constructor(fs=filesystem, **kwargs)
 
 
-def patch_to_dump_ordered_dicts_as_dicts(dumper_cls: DumperType):
-    """Patches a Dumper to not dump OrderedDicts as python objects, but regular dicts."""
+def patch_to_dump_ordered_dicts_as_dicts(dumper_cls: DumperType) -> None:
+    """Patch a Dumper to handle OrderedDicts as regular dicts.
+
+    Args:
+        dumper_cls: YAML dumper class to patch
+    """
 
     def map_representer(dumper: yaml.Dumper, data: dict[Any, Any]) -> yaml.MappingNode:
         return dumper.represent_dict(data.items())
 
-    dumper_cls.add_representer(dict, map_representer)
-    dumper_cls.add_representer(collections.OrderedDict, map_representer)
+    for dict_type in (dict, collections.OrderedDict):
+        dumper_cls.add_representer(dict_type, map_representer)
 
 
-def get_safe_loader(base_loader_cls: LoaderType):
-    """Return a "SafeLoader" based on given loader.
+def get_safe_loader(base_loader_cls: LoaderType) -> LoaderType:
+    """Create a SafeLoader with dummy constructors for common tags.
 
-    The new loader possesses additional dummy constructors for some commonly used tags.
+    Args:
+        base_loader_cls: Base loader class to extend
 
-    Arguments:
-        base_loader_cls: The loader class to derive the new loader from
+    Returns:
+        Enhanced safe loader class
     """
     loader_cls = create_subclass(base_loader_cls)
-    tags = ["!include", "!relative"]
-    for tag in tags:
+
+    # Add dummy constructors for simple tags
+    for tag in ("!include", "!relative"):
         loader_cls.add_constructor(tag, lambda loader, node: None)
-    tags = ["tag:yaml.org,2002:python/name:", "tag:yaml.org,2002:python/object/apply:"]
-    for tag in tags:
+
+    # Add dummy constructors for complex tags
+    python_tags = (
+        "tag:yaml.org,2002:python/name:",
+        "tag:yaml.org,2002:python/object/apply:",
+    )
+    for tag in python_tags:
         loader_cls.add_multi_constructor(tag, lambda loader, suffix, node: None)
     # https://github.com/smart-home-network-security/pyyaml-loaders/
     # loader_cls.add_multi_constructor("!", lambda loader, suffix, node: None)
@@ -90,14 +110,20 @@ def get_safe_loader(base_loader_cls: LoaderType):
 def get_loader(
     base_loader_cls: LoaderType,
     include_base_path: str | os.PathLike[str] | fsspec.AbstractFileSystem | None = None,
-):
-    """Construct a loader based on given options.
+) -> LoaderType:
+    """Construct an enhanced YAML loader with support for !env and !include tags.
 
-    By default !env and !include tags are added to the loader.
-    See https://github.com/waylan/pyyaml-env-tag
+    Args:
+        base_loader_cls: Base loader class to extend
+        include_base_path: Base path for !include tag resolution
+
+    Returns:
+        Enhanced loader class
     """
     loader_cls = create_subclass(base_loader_cls)
     constructor = get_include_constructor(fs=include_base_path)
+
+    # Add constructors for special tags
     yaml.add_constructor("!include", constructor, loader_cls)
     loader_cls.add_constructor("!ENV", yaml_env_tag.construct_env_tag)
     loader_cls.add_constructor("!include", yaml_include.Constructor())
@@ -108,34 +134,36 @@ def load_yaml(
     text: str,
     mode: LoaderStr = "unsafe",
     include_base_path: str | os.PathLike[str] | fsspec.AbstractFileSystem | None = None,
-):
-    """Load a yaml string.
+) -> Any:
+    """Load a YAML string with specified safety mode and include path support.
 
-    Arguments:
-        text: the string to load
-        mode: the yaml loader mode.
-        include_base_path: Base path to use for !include tag
+    Args:
+        text: YAML content to parse
+        mode: Loading mode determining safety level
+        include_base_path: Base path for resolving !include tags
+
+    Returns:
+        Parsed YAML content
     """
-    match mode:
-        case "unsafe":
-            base_loader_cls: type = yaml.CUnsafeLoader
-        case "full":
-            base_loader_cls = yaml.CFullLoader
-        case _:
-            base_loader_cls = yaml.CSafeLoader
-
-    # Derive from global loader to leave the global loader unaltered.
+    base_loader_cls: type = LOADERS[mode]
     loader = get_loader(base_loader_cls, include_base_path=include_base_path)
     return yaml.load(text, Loader=loader)
 
 
-def dump_yaml(obj: Any, ordered_dict_as_dict: bool = False, **kwargs: Any) -> str:
-    """Dump a data structure to a yaml string.
+def dump_yaml(
+    obj: Any,
+    ordered_dict_as_dict: bool = False,
+    **kwargs: Any,
+) -> str:
+    """Dump a data structure to a YAML string.
 
-    Arguments:
-        obj: The object to serialize
-        ordered_dict_as_dict: Whether to dump OrderedDict as dict
-        kwargs: Keyword arguments passed to yaml.dump
+    Args:
+        obj: Object to serialize
+        ordered_dict_as_dict: Whether to treat OrderedDict as regular dict
+        kwargs: Additional arguments for yaml.dump
+
+    Returns:
+        YAML string representation
     """
     dumper_cls = create_subclass(yaml.Dumper)
     if ordered_dict_as_dict:
@@ -146,8 +174,10 @@ def dump_yaml(obj: Any, ordered_dict_as_dict: bool = False, **kwargs: Any) -> st
 if __name__ == "__main__":
     from collections import OrderedDict
 
-    data = OrderedDict([("b", 2), ("a", 1)])
-    test = dump_yaml(data)
-    print(test)
-    cfg = load_yaml(test)
-    print(fsspec.url_to_fs("C:/teset"))
+    test_data = OrderedDict([("b", 2), ("a", 1)])
+    yaml_str = dump_yaml(test_data)
+    print(yaml_str)
+    loaded_cfg = load_yaml(yaml_str)
+    print(fsspec.url_to_fs("C:/test"))
+
+    print(fsspec.url_to_fs("C:/test"))
